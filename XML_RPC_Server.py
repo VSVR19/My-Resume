69dcc83e25167ea34a581f8bcb3df3edb6543473
VSVR19 <35869108+VSVR19@users.noreply.github.com>
2019-02-08 11:27:11 -0600

    XMLs!

--- XML_RPC_Server.py
+++ XML_RPC_Server.py
@@ -14,12 +14,17 @@ try:
 		print("Connected with client number " + str(client_count))
 		return "Connected with client number " + str(client_count)
 
+	def client_message():
+		return("Hi from Server")
+
 	server = SimpleXMLRPCServer(("127.0.0.1", 2347))
 	print("Listening on port 2347...")
+
 	server.register_function(is_even, "is_even")
 	server.register_function(connected_clients, "connected_clients")
+	server.register_function(client_message, "client_message")
+	
 	server.serve_forever()
 
 except:
 	print("Error logs caught by exception handlers")
-
